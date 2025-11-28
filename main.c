f461014cc1c95c002204f38f61257a26c90bbe2e
justxmax <sofroni.max@gmail.com>
2025-11-28 16:41:51 +0200

    Am corectat functionalitatea scriptului si am termiant problema main.c

--- main.c
+++ main.c
@@ -4,44 +4,58 @@
 #include <ctype.h>
 
 int f1(char **addr_p){
-	int *upper = 0;
-	int *lower = 0;
+	char *upper = NULL;
+	char *lower = NULL;
 	int size = 0;
 	int capacity = 10; // consideram pentru toate sirurile aceeasi lungime
 	int lowCap = 10;
 	int upCap = 10;
 	int count_mari = 0;
 	int count_mici = 0; 
-	*addr_p = (int *)calloc(capacity , sizeof(char));
-	if(*addr_p == NULL){
-		printf("Eroare la alocarea memoriein\n");
-		return -1;
-	}
+	*addr_p = (char *)calloc(capacity , sizeof(char));
 	upper = (char *)calloc(capacity , sizeof(char));
 	lower = (char *)calloc(capacity , sizeof(char));
+	if(*addr_p == NULL || upper == NULL || lower == NULL){
+		printf("Eroare la alocarea memoriei\n");
+		free(*addr_p);
+		free(lower);
+		free(upper);
+		return -1;
+	}
 	char litera;
+
 	while(scanf("%c" , &litera) == 1){
 		if(litera == '\n'){
 			break;
 		}
 		if(size == capacity){
-			int *temp ;
+			char *temp ;
 			capacity *=2;
 			temp = (char *)realloc(*addr_p , capacity * sizeof(char));
 			if(temp == NULL){
 				printf("Eroare la realocarea memoriei\n");
+				free(*addr_p);
+				*addr_p = NULL;
+				free(upper);
+				free(lower);
 				return -1;
-			}else{
-				*addr_p = temp;
-				// free(temp);
 			}
+			*addr_p = temp;		
 		}
+		(*addr_p)[size++] = litera;
+
+
+
 		if(count_mici == lowCap){
-			int *lowTemp;
+			char *lowTemp;
 			lowCap *=2;
 			lowTemp = (char *)realloc(lower , lowCap * sizeof(char));
 			if(lowTemp == NULL){
 				printf("Eroare la realocarea memoriei\n");
+				free(*addr_p);
+				*addr_p = NULL;
+				free(upper);
+				free(lower);
 				return -1;
 			}else{
 				lower = lowTemp;
@@ -53,51 +67,70 @@ int f1(char **addr_p){
 
 
 		if(count_mari == upCap){
-			int *upTemp;
+			char *upTemp;
 			upCap *= 2;
 			upTemp = (char *)realloc(upper , upCap * sizeof(char));
 			if(upTemp == NULL){
 				printf("Eroare la realocarea memoriei\n");
-				return -1;
+				free(*addr_p);
+				*addr_p = NULL;
+				free(upper);
+				free(lower);
 			}
 		}
 		if(isupper(litera)){
 			upper[count_mari++] = litera;
 		}
-		*(addr_p)[size++] = litera;
 	}
 	//end while
 
 
+	//sortare upper
 	for(int i = 0 ;i  < count_mari - 1; i++){
 		for(int j = i + 1 ; j < count_mari ; j++){
 			if(upper[i] > upper[j]){
-				int aux = upper[i];
+				char aux = upper[i];
 				upper[i] = upper[j];
 				upper[j] = aux;
 			}
 		}
 	}
-	for(int i = 0 ; i < count_mici  -1; i++){
+
+	//sortare lower
+	for(int i = 0 ; i < count_mici - 1; i++){
 		for(int j = i + 1 ; j < count_mici ; j++){
 			if(lower[i] > lower[j]){
-				int aux = lower[i];
+				char aux = lower[i];
 				lower[i] = lower[j];
 				lower[j] = aux ;
 			}
 		}
 	}
-
-
-
-
+	int index_mare = 0;
+	int index_mic = 0;
+	for(int i = 0 ; i < size ; i++){
+		if(isupper((*addr_p)[i])){
+			if(index_mare < count_mari){
+				(*addr_p)[i] = upper[index_mare++];
+			}
+		}else if(islower((*addr_p)[i])){
+			if(index_mic < count_mici){
+				(*addr_p)[i] = lower[index_mic++];
+			}
+		}
+	}
+	free(upper);
+	free(lower);
 	return abs(count_mari - count_mici);
 }
 
 int main(void){
-	char *sir;
-	int *p = &sir;
-	int k = f1(&p);
-	printf("%d" , k);
+	char *sir = NULL;
+	int k = f1(&sir);
+	if(k != -1 && sir != NULL){
+		printf("Sirul modificat este : %s\n" , sir);
+		free(sir);
+	}
+	printf("Diferenta in modul dintre literele mari si literele mici: %d\n" , k);
 	return 0;
 }
--- script.sh
+++ script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #se ruleaza deocamdata doar pentur test si main
-value=$1
+value="$1"
 
-if [[ $value="test" ]]; then
+if [[ "$value" == "test" ]]; then
     gcc test.c -o test
     ./test
     rm -rf test
-elif [[ $value="main" ]]; then
-    gcc main.c -o test
+	echo "test.c a rulat cu scucces"
+elif [[ "$value" == "main" ]]; then
+    gcc main.c -o main
     ./main
     rm -rf main
+	echo "main.c a rulat cu scucces"
 fi
